55d0fbcbf2d8b8432f421eed00779624a646230e
Obasan Tolulope <obasantolu@gmail.com>
2025-08-14 13:21:08 +0100

    removed unnecessary components

--- src/components/Dashboard/SearchPage.tsx
+++ src/components/Dashboard/SearchPage.tsx
@@ -3,25 +3,21 @@ import { Link } from "react-router-dom";
 const SearchPage = () => {
   return (
     <div className="max-h-screen flex flex-col items-center justify-center px-4">
-      {/* Illustration */}
       <img
         src="/Trip-rafiki.svg"
         alt="Travel illustration"
         className="w-72 md:w-96 mb-6"
       />
 
-      {/* Headline */}
       <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-4">
         Coming Soon
       </h1>
 
-      {/* Short Description */}
       <p className="text-gray-600 text-center max-w-md mb-6">
         We’re preparing something exciting to help you discover destinations
         better.
       </p>
 
-      {/* Button */}
       <Link
         to="/Recommendations"
         className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-all"
